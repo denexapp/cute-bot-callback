50812afbad0218c4cb4047c8b7d99d1d9e25269c
Denis Mukhametov <denis.mukhametov@gmail.com>
2021-05-06 19:35:27 +0500

    Improve kick command

--- commands/kick.ts
+++ commands/kick.ts
@@ -1,4 +1,5 @@
 import { JsonDecoder } from "ts.data.json";
+import { VkError, VkErrorCode } from "vk-ts";
 import { Command, CommandObject } from ".";
 import callbackDecoder, { Callback } from "../utils/callbackDecoder";
 import vk from "../utils/vk";
@@ -10,14 +11,39 @@ type Params = {
   userId: number;
 };
 
-type Result = null;
+enum Status {
+  Kicked = 0,
+  UserIsAnAdmin = 1,
+  NoUserInChat = 2,
+}
+
+type Result = {
+  status: Status;
+};
 
 const type: Type = "kick";
 
 const command: Command<Params, Result> = async (params) => {
   const { chatId, userId } = params;
-  await vk.messagesRemoveChatUser(chatId, userId);
-  return null;
+  try {
+    await vk.messagesRemoveChatUser(chatId, userId);
+  } catch (error) {
+    if (!(error instanceof VkError)) throw error;
+    if (error.code === VkErrorCode.NoAccess) {
+      return {
+        status: Status.UserIsAnAdmin,
+      };
+    }
+    if (error.code === VkErrorCode.NoUserInChat) {
+      return {
+        status: Status.NoUserInChat,
+      };
+    }
+    throw error;
+  }
+  return {
+    status: Status.Kicked,
+  };
 };
 
 const decoder: JsonDecoder.Decoder<Callback<Type, Params>> = callbackDecoder(
